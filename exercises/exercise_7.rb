63f914453f8509f92647e3aac195f107e154c42d
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-27 15:54:38 -0700

    logic complete, testing is next step

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  #validations
+  validates :first_name, :last_name, :store, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+
+  #validations
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_carry_apparel
+
+  private
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry at least men's or women's apparel")
+    end
+  end
+end
